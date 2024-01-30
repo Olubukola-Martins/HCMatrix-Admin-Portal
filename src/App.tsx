9e9faab8b700585d0af32c412fa467ee8c568f9c
kaasikodes <thisbroisfresh@gmail.com>
2024-01-30 18:11:15 +0100

    chore: adds app providers to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import AppLayout from "components/layouts/AppLayout";
+import { AppProviders } from "components/providers";
 
 function App() {
   return (
-    <>
+    <AppProviders>
       <AppLayout>
         <div className="text-red-600">red</div>
       </AppLayout>
-    </>
+    </AppProviders>
   );
 }
 
