82bb158488d735c528d62f857c66e533102755d5
kaasikodes <thisbroisfresh@gmail.com>
2024-02-27 13:59:22 +0100

    chore: removes unused form fields from vat form

--- src/modules/settings/components/prices/vat/VatForm.tsx
+++ src/modules/settings/components/prices/vat/VatForm.tsx
@@ -1,8 +1,6 @@
-import { Button, Form, Input, Segmented, Select, Skeleton } from "antd";
-import { billingCycleOptions, currencyOptions } from "constants";
+import { Button, Form, Input, Skeleton } from "antd";
 import { useGetVat } from "lib/api/subscription/vat/get-vat";
-import { useEffect, useState } from "react";
-import { TBillingCycle, TCurrency } from "types";
+import { useEffect } from "react";
 
 const VatForm: React.FC<{
   onSubmit: {
@@ -11,13 +9,7 @@ const VatForm: React.FC<{
   };
 }> = ({ onSubmit }) => {
   const [form] = Form.useForm<{ value: number }>();
-  const [selection, setSelection] = useState<{
-    billingCycle: TBillingCycle;
-    currency: TCurrency;
-  }>({
-    billingCycle: "monthly",
-    currency: "usd",
-  });
+
   const { data, isLoading } = useGetVat();
   useEffect(() => {
     form.setFieldsValue({
@@ -27,31 +19,6 @@ const VatForm: React.FC<{
   return (
     <Skeleton active loading={isLoading} paragraph={{ rows: 4 }}>
       <Form form={form} labelCol={{ span: 24 }} onFinish={onSubmit.fn}>
-        <div className="flex justify-end gap-x-4 mb-6">
-          <Select
-            options={billingCycleOptions.map((item) => ({
-              value: item,
-              label: <span className="capitalize">{item}</span>,
-            }))}
-            className="min-w-24"
-            value={selection.billingCycle}
-            onSelect={(v) => setSelection({ ...selection, billingCycle: v })}
-          />
-          <Segmented
-            options={currencyOptions.map((item) => ({
-              value: item,
-              label: <span className="uppercase">{item}</span>,
-            }))}
-            value={selection.currency}
-            onChange={(v) =>
-              setSelection({
-                ...selection,
-                currency: v as unknown as TCurrency,
-              })
-            }
-          />
-        </div>
-
         <Form.Item
           name={`value`}
           label={<span className="text-lg font-medium">Value in %</span>}
